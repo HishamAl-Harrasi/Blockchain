f851c09332123ec3c1cf3ccdfe521d3cccab300f
Hisham Al-Harrasi <u1840156@live.warwick.ac.uk>
2021-09-14 16:04:11 +0100

    Fixed the issue with the serialization and hashing, now hashing a block works correctly. Next step is to set up basic mining capability

--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -9,19 +9,19 @@
 class Blockchain{
     private:
         std::vector <Block> chain;
+        
+        std::ostringstream serializeBlock(Block* block);
     public:
         Blockchain();
 
         void addBlock(Block block);
 
-        // const unsigned char* hashBlock();
-        void hashBlock(Block* block);
+        unsigned char* hashBlock(Block* block);
 
-        const char* serializeBlock(Block* block);
 };
 
 
-// Constructor: On construction fo the blockchain, the chain should automatically include the genesis block. The genesis block is special because there
+// Constructor: On construction of the blockchain, the chain should automatically include the genesis block. The genesis block is special because there
 // are no blocks behind it, and therefore the prevBlockHash attribute will not be filled with a real hash of a previous block 
 Blockchain::Blockchain(){
      
@@ -32,7 +32,7 @@ void Blockchain::addBlock(Block block){
     chain.push_back(block);
 }
 
-const char* Blockchain::serializeBlock(Block* block){
+std::ostringstream Blockchain::serializeBlock(Block* block){
     
     std::ostringstream serializedBlockStream;
 
@@ -44,22 +44,23 @@ const char* Blockchain::serializeBlock(Block* block){
     for (int i = 0; i < blockTransactions.size(); i++){
         serializedBlockStream << blockTransactions[i];
     }
+
+    return serializedBlockStream;
+}
+
+unsigned char* Blockchain::hashBlock(Block* block){
+    
+    std::ostringstream serializedBlockStream = serializeBlock(block);
     
     std::string serializedBlockString = serializedBlockStream.str();
 
     const char* serializedBlock = serializedBlockString.c_str();
-    
-    return serializedBlock;
-}
+    uint32_t strLength = strlen(serializedBlock);
 
-// const unsigned char* Blockchain::hashBlock(){
-void Blockchain::hashBlock(Block* block){
-        // LATEST ISSUE - for some reason running serializeBlock() does not return anything at the moment - probably problem with pointers
-    const char* test = serializeBlock(block);
+    unsigned char* hash;
+    hasher::sha256(serializedBlock, strLength, hash);
 
-    std::cout << test[0] << std::endl;
-
-    // hasher::sha256()
+    return hash;    
 }
 
 int main(){
